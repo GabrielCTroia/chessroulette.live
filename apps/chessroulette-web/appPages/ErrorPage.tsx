5ca857541cb7bacf0cd674fc3f204b668805ba2d
Gabriel C. Troia <catalin.troia@gmail.com>
2024-03-03 23:08:37 -0600

    Allow the error page to show logs

--- apps/chessroulette-web/appPages/ErrorPage.tsx
+++ apps/chessroulette-web/appPages/ErrorPage.tsx
@@ -4,11 +4,15 @@ type Props = {
   error?: Error & { digest?: string };
 };
 
-export const ErrorPage = ({ error }: Props) => (
-  <div className="flex flex-1 items-center justify-center h-screen w-screen text-lg divide-x">
-    <div className="flex flex-col gap-4 items-center">
-      <h2 className="text-2xl">Something went wrong!</h2>
-      {config.DEV_MODE && <pre>{JSON.stringify(error, null, 2)}</pre>}
+export const ErrorPage = ({ error }: Props) => {
+  console.error('ErrorPage', error);
+
+  return (
+    <div className="flex flex-1 items-center justify-center h-screen w-screen text-lg divide-x">
+      <div className="flex flex-col gap-4 items-center">
+        <h2 className="text-2xl">Something went wrong!</h2>
+        {config.DEV_MODE && <pre>{JSON.stringify(error, null, 2)}</pre>}
+      </div>
     </div>
-  </div>
-);
+  );
+};
